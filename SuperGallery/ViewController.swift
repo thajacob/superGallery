3a1b5b34077952baa3c6b7d75b78624215ffd148
thajacob <tha.jacob2@gmail.com>
2017-06-14 10:07:00 +0100

    Initial Commit

---
+++ SuperGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperGallery
+//
+//  Created by jakub skrzypczynski on 14/06/2017.
+//  Copyright © 2017 test project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
